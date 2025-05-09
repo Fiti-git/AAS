d5bca93ae9f49f1dab3744737f38078b0fe2923c
Fiti-git <work@fathitech.com.lk>
2025-05-09 20:34:45 +0530

    ALLOWED_HOSTS = ['128.199.24.138', 'localhost', '127.0.0.1']

--- aas/settings.py
+++ aas/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-0o(b@duchc*na=6x3022%zag8y(*m5#s#z%gq)1ng6@-p74dih
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['128.199.24.138', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
