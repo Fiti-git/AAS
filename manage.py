6657c5f0d30c193c7852a7cfd4779b165eb91cc1
Fiti-git <work@fathitech.com.lk>
2025-05-10 11:21:09 +0530

    restre

--- manage.py
+++ manage.py
@@ -2,29 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.core.management import execute_from_command_line
-from django.core.management import call_command
-from django.contrib.auth import get_user_model
-
-
-def create_superuser():
-    """Create superuser if one doesn't exist."""
-    User = get_user_model()
-    username = 'admin'
-    password = 'adminpassword'
-    email = 'admin@example.com'
-
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, password=password, email=email)
-        print("Superuser created successfully!")
-    else:
-        print("Superuser already exists.")
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aas.settings')
-    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -33,11 +15,6 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-
-    # Call the function to create the superuser
-    create_superuser()
-
-    # Execute the Django management command (e.g., runserver, migrate, etc.)
     execute_from_command_line(sys.argv)
 
 
