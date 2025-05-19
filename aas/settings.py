af52cc97b50e500e5d2c6120c64576c0f21661f8
Fiti-git <work@fathitech.com.lk>
2025-05-19 22:46:49 +0530

    ALLOWED_HOSTS = ['*']

--- aas/settings.py
+++ aas/settings.py
@@ -12,7 +12,8 @@ SECRET_KEY = 'django-insecure-0o(b@duchc*na=6x3022%zag8y(*m5#s#z%gq)1ng6@-p74dih
 DEBUG = True
 
 #ALLOWED_HOSTS = ['128.199.24.138', 'localhost', '127.0.0.1', 'arunalusupermarket.shop', '64.227.181.72']
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'arunalusupermarket.shop', '64.227.181.72']
+ALLOWED_HOSTS = ['*']
+
 
 
 # ✅ Add CORS settings
