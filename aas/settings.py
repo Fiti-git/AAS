8f57fdfc7737e25cb831630a22135fa38a6461d2
Fiti-git <work@fathitech.com.lk>
2025-05-19 22:20:55 +0530

    ALLOWED_HOSTS = ['*'] given to all

--- aas/settings.py
+++ aas/settings.py
@@ -11,7 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0o(b@duchc*na=6x3022%zag8y(*m5#s#z%gq)1ng6@-p74dih'
 DEBUG = True
 
-ALLOWED_HOSTS = ['128.199.24.138', 'localhost', '127.0.0.1', 'arunalusupermarket.shop']
+ALLOWED_HOSTS = ['*']
+
 
 # ✅ Add CORS settings
 INSTALLED_APPS = [
