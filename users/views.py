ceb199a5f7fa755678c1e8c86f0acfacf818160e
Fiti-git <work@fathitech.com.lk>
2025-06-14 19:22:48 +0530

    path cehaing the models.py file in the views.py

--- users/views.py
+++ users/views.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
-from .models import UserDevice
+from main.models import UserDevice
+
 
 class CustomTokenObtainpairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
