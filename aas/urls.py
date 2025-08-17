5d89b64321a8702a112f2cc2d81833d8dc899d0b
Fiti-git <work@fathitech.com.lk>
2025-08-17 12:08:05 +0530

    just try to acess the images via url

--- aas/urls.py
+++ aas/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from main import views
@@ -51,3 +53,6 @@ urlpatterns = [
     path('api/users/', include('users.apiurls')),
     path('admintool/', include('face_recognition.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
