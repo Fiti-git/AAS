1f8357c2638d835635dad25be2919df52ed462fe
Fiti-git <work@fathitech.com.lk>
2026-01-26 10:52:50 +0530

    Add AllowAny permission to CustomTokenObtainpairView for enhanced access control

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
-from users.serializers import CustomTokenObtainPairSerializer
+from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
+from users.serializers import CustomTokenObtainPairSerializer
 
 class CustomTokenObtainpairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+    permission_classes = [AllowAny]   # 👈 THIS LINE FIXES IT
